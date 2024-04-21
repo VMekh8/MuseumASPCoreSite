adabe4e271f868a10d7fa2d017ee76c9b5b0013e
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-21 14:13:51 +0300

    Add packages and create user model
    Add an Entity Framework Core
    Add an ASP.NETCore.Identity

---
+++ MuseumSite.Core/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MuseumSite.Core.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get;} = string.Empty;
+        public string LastName { get;} = string.Empty;
+
+        private User(string name, string surname)
+        {
+            FirstName = name;
+            LastName = surname;
+        }
+
+        public static (User User, string Error) CreateUser(string name, string lastname)
+        {
+            string Error = string.Empty;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Error = "Name format error";
+            }
+            else if (string.IsNullOrEmpty(lastname))
+            {
+                Error = "Surname format error";
+            }
+
+            var user = new User(name, lastname);
+            return (user, Error);
+        }
+
+    }
+}
