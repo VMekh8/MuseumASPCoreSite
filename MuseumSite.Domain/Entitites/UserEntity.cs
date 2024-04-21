ad6f5caf2a309ba89d91e20ac6781134a655c10c
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-21 14:58:37 +0300

    Add connection to SQL Server

---
+++ MuseumSite.Domain/Entitites/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MuseumSite.Domain.Entitites
+{
+    public class UserEntity : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ MuseumSite.Domain/MuseumDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MuseumSite.Domain.Entitites;
+
+namespace MuseumSite.Domain
+{
+    public class MuseumDbContext : IdentityDbContext<UserEntity>
+    {
+        public MuseumDbContext(DbContextOptions<MuseumDbContext> options) : base (options)
+        {
+        }
+
+        public DbSet<ExhibitionEntity> ExhibitionEntity { get; set; }
+        public DbSet<ExhitbitEntity> ExhitbitEntity { get; set;}
+        public DbSet<MuseumNewsEntity> MuseumNewsEntity { get; set; }
+
+    }
+}
