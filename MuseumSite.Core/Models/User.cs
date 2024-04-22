a1e7cde23db4397b4336a7768001f33564e235b8
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-22 16:02:03 +0300

    Remake User Model

--- MuseumSite.Core/Models/User.cs
+++ MuseumSite.Core/Models/User.cs
@@ -7,17 +7,23 @@ namespace MuseumSite.Core.Models
         public string FirstName { get;} = string.Empty;
         public string LastName { get;} = string.Empty;
 
-        private User(string name, string surname)
+        private User(string email, string password, string? phoneNumber, string firstname, string lastname)
         {
-            FirstName = name;
-            LastName = surname;
+            Email = email;
+            PasswordHash = password;
+            PhoneNumber = phoneNumber;
+            FirstName = firstname;
+            LastName = lastname;
         }
 
-        public static (User User, string Error) CreateUser(string name, string lastname)
+        public static (User User, string Error) CreateUser(string email, string password, string? phoneNumber, string firstname, string lastname)
         {
             string Error = string.Empty;
-
-            if (string.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                Error = "User data error, check for login or password";
+            }
+            else if (string.IsNullOrEmpty(firstname))
             {
                 Error = "Name format error";
             }
@@ -26,7 +32,7 @@ namespace MuseumSite.Core.Models
                 Error = "Surname format error";
             }
 
-            var user = new User(name, lastname);
+            var user = new User(email, password, phoneNumber, firstname, lastname);
             return (user, Error);
         }
 
