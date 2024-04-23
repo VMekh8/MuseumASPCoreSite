2281bd5bc222cbee16905aa26cd586f1582fd2b8
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-23 19:38:43 +0300

    Make Museum News Service and extract interface

---
+++ MuseumSite.Core/Abstract/IMuseumNewsService.cs
@@ -0,0 +1,13 @@
+﻿using MuseumSite.Core.Models;
+
+namespace MuseumSite.Application.Services
+{
+    public interface IMuseumNewsService
+    {
+        Task<Guid> CreateNewsAsync(MuseumNews museumNews);
+        Task<Guid> DeleteNewsAsync(Guid id);
+        Task<List<MuseumNews>> GetAllNewsAsync();
+        Task<MuseumNews> GetNewsByIdAsync(Guid id);
+        Task<Guid> UpdateNewsAsync(MuseumNews news);
+    }
+}
