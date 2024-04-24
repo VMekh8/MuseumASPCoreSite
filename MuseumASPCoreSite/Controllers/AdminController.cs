67a04fda3c0aef34e4eff59057d49131f3ea133f
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-24 20:50:15 +0300

    Make GetAllExhibits API

--- MuseumASPCoreSite/Controllers/AdminController.cs
+++ MuseumASPCoreSite/Controllers/AdminController.cs
@@ -1,6 +1,24 @@
-﻿namespace MuseumASPCoreSite.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MuseumASPCoreSite.Contracts;
+using MuseumSite.Application.Services;
+using MuseumSite.Core.Models;
+
+namespace MuseumASPCoreSite.Controllers
 {
-    public class AdminController
+    [ApiController]
+    [Route("[controller]")]
+    //[Authorize(Roles ="Admin")]
+    public class AdminController : ControllerBase
     {
+        private readonly IExhibitService _exhibitService;
+
+        public AdminController(IExhibitService exhibitService)
+        {
+            _exhibitService = exhibitService;
+        }
+
+       
+        
     }
 }
--- MuseumASPCoreSite/Controllers/ClientController.cs
+++ MuseumASPCoreSite/Controllers/ClientController.cs
@@ -1,6 +1,55 @@
-﻿namespace MuseumASPCoreSite.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MuseumASPCoreSite.Contracts;
+using MuseumSite.Application.Services;
+
+namespace MuseumASPCoreSite.Controllers
 {
-    public class ClientController
+    [ApiController]
+    [Route("[controller]")]
+    //[Authorize(Roles ="Client, Worker, Admin")]
+    public class ClientController : ControllerBase
     {
+        private readonly IExhibitService _exhibitService;
+        private readonly IExhibitionService _exhibitionService;
+        private readonly IMuseumNewsService _museumNewsService;
+
+
+        public ClientController(IExhibitService exhibitService, IExhibitionService exhibitionService, IMuseumNewsService museumNewsService)
+        {
+            _exhibitService = exhibitService;
+            _exhibitionService = exhibitionService;
+            _museumNewsService = museumNewsService;
+        }
+
+        [HttpGet("GetAllExhibit")]
+        public async Task<ActionResult<List<ExhibitResponce>>> GetAllExhibits()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var exhibitsModel = await _exhibitService.GetExhibitsAsync();
+
+            if (exhibitsModel.Count <= 0)
+            {
+                return Ok("Exhibits collection is empty");
+            }
+
+            if (exhibitsModel.Count > 0)
+            {
+                var exhibitToSend = exhibitsModel.Select(e => new ExhibitResponce(
+                    e.Id,
+                    e.Title,
+                    e.Description,
+                    Convert.ToBase64String(e.Image),
+                    e.ExhibitionId));
+
+                return Ok(exhibitToSend);
+            }
+
+            return BadRequest("Exhibits get error");
+        }
     }
 }
