7ce92ada1bc422f23db92a43d21aec992245cbbd
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-27 08:53:10 +0300

    Add Delete Exhibit API

--- MuseumASPCoreSite/Controllers/DeleteController.cs
+++ MuseumASPCoreSite/Controllers/DeleteController.cs
@@ -1,6 +1,42 @@
-﻿namespace MuseumASPCoreSite.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MuseumSite.Application.Services;
+
+namespace MuseumASPCoreSite.Controllers
 {
-    public class DeleteController
+    [ApiController]
+    [Route("[controller]")]
+    //[Authorize(Roles ="Worker, Admin")]
+    public class DeleteController : ControllerBase
     {
+        private readonly IExhibitService _exhibitService;
+        private readonly IExhibitionService _exhibitionService;
+        private readonly IMuseumNewsService _museumNewsService;
+
+
+        public DeleteController(IExhibitService exhibitService, IExhibitionService exhibitionService, IMuseumNewsService museumNewsService)
+        {
+            _exhibitService = exhibitService;
+            _exhibitionService = exhibitionService;
+            _museumNewsService = museumNewsService;
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<int>> DeleteExhibit(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _exhibitService.DeleteExhibitAsync(id);
+
+            if (result > 0)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest("Exhibit delete error");
+        }
     }
 }
