8474f4cdae50ac5f54b41e584569c81e8c778f72
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-27 16:06:52 +0300

    Make add and delete exhibit to exhibition

---
+++ MuseumASPCoreSite/Controllers/ExhibitonController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MuseumASPCoreSite.Contracts;
+using MuseumASPCoreSite.Contracts.Requests;
+using MuseumSite.Application.Services;
+using MuseumSite.Core.Models;
+
+namespace MuseumASPCoreSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExhibitonController : ControllerBase
+    {
+        private readonly IExhibitionService _exhibitionService;
+        private readonly IExhibitService _exhibitService;
+
+        public ExhibitonController(IExhibitService exhibitService, IExhibitionService exhibitionService)
+        {
+            _exhibitService = exhibitService;
+            _exhibitionService = exhibitionService;
+        }
+
+        
+
+        [HttpPost("AddExhibitToExhibition")]
+        public async Task<ActionResult<int>> AddExhibitToExhibition(int exhibitionId, int exhibitId)
+        {
+            return await _exhibitionService.AddExhibitToExhibition(exhibitionId, exhibitId);
+        }
+
+        [HttpDelete("DeleteExhibitToExhibition")]
+        public async Task<ActionResult<int>> DeleteExhibitToExhibition(int exhibitionId, int exhibitId)
+        {
+            return await _exhibitionService.DeleteExhibitFromExhibition(exhibitionId, exhibitId);
+        }
+    }
+}
