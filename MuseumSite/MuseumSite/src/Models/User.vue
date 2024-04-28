a976b0ab6cdd4531c6ff94e64cf07793caa429fd
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-28 16:00:06 +0300

    Create an UserResponce model

---
+++ MuseumSite/MuseumSite/src/Models/User.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+export class UserResponce {
+    id: string;
+    email: string;
+    password: string;
+    phonenumber: string | null;
+    firstname: string;
+    lastname: string;
+
+    constructor (id: string, email: string, password: string, phonenumber: string | null, firstname: string, lastname: string) {
+        this.id = id;
+        this.email = email;
+        this.password = password; 
+        this.phonenumber = phonenumber;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+}
+</script>
