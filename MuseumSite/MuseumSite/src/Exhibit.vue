92d0b7c67d81aa3f431aad5a6164e6c831b214b0
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-28 14:59:13 +0300

    Add a exhibitiRequest class

--- MuseumSite/MuseumSite/src/Exhibit.vue
+++ MuseumSite/MuseumSite/src/Exhibit.vue
@@ -14,4 +14,21 @@ export class ExibitResponce {
         this.exhibitionId = exhibitionId;
     }
 }
+
+export class ExhibitRequest {
+    id: number;
+    title: string;
+    description: string;
+    image: File | null;
+    exhibitionId: number | null;
+
+    constructor (id: number, title: string, desc: string, image: File | null, exhibId: number | null)
+    {
+        this.id = id;
+        this.title = title;
+        this.description = desc;
+        this.image = image;
+        this.exhibitionId = exhibId;
+    }
+}
 </script>
