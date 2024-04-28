dbd81cfaccd4bce9dc7157e86da237d3faef9017
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-28 15:12:03 +0300

    Add an MuseumNewsResponce class

---
+++ MuseumSite/MuseumSite/src/Models/MuseumNews.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export class MuseumNewsResponce {
+    id: number;
+    title: string;
+    description: string;
+    image: string;
+
+    constructor(id: number, title: string, desc: string, image: string) {
+        this.id = id;
+        this.title = title;
+        this.description = desc;
+        this.image = image;
+    }
+}
+</script>
