3cc5881d3e82f8c092ae6a486fae026f6c700959
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-28 15:15:44 +0300

    add a MuseumNewsRequest class

--- MuseumSite/MuseumSite/src/Models/MuseumNews.vue
+++ MuseumSite/MuseumSite/src/Models/MuseumNews.vue
@@ -12,4 +12,18 @@ export class MuseumNewsResponce {
         this.image = image;
     }
 }
+
+export class MuseumNewsRequest {
+    id: number;
+    title: string;
+    description: string;
+    image: File | null;
+
+    constructor (id: number, title: string, desc: string, image: File | null) {
+        this.id = id;
+        this.title = title;
+        this.description = desc;
+        this.image = image;
+    }
+}
 </script>
