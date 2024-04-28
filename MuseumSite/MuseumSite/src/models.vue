a0c640befdcd9825e7cedf8d8b118848f387d0b1
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-28 14:55:53 +0300

    Add an ExhibitResponce model

---
+++ MuseumSite/MuseumSite/src/models.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export class ExibitResponce {
+    id: number;
+    title: string;
+    description: string;
+    image: string;
+    exhibitionId : number | null;
+
+    constructor(id: number, title: string, description: string, image: string, exhibitionId: number | null) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.exhibitionId = exhibitionId;
+    }
+}
+</script>
