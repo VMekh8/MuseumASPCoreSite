8833a445b617eed21a8406cb2cbc83cdeb2835ea
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-28 17:25:23 +0300

    Make a test Login page

---
+++ MuseumSite/MuseumSite/src/layouts/UserLayout/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <input type="email" id="email" v-model="email">
+        <input type="password" id="password" v-model="password">
+        <button type="submit" v-on:click="Login">Login</button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async Login() {
+            try {
+                const responce = await axios.post('https://localhost:7105/Account/Login', {
+                    email: this.email,
+                    password: this.password
+                });
+
+                if (responce.status === 200) {
+                    console.log(responce.data.token);
+                }
+            }
+            catch(error) {
+                console.error(error);
+            }
+        }
+    }
+}
+</script>
