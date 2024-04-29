604f754a4b5c3134c6f4c9ac64d8fc3cc08d47c8
VMekh8 <mekhvitaliy88@gmail.com>
2024-04-29 22:25:53 +0300

    Start working with vue-router

--- MuseumSite/MuseumSite/src/main.ts
+++ MuseumSite/MuseumSite/src/main.ts
@@ -1,11 +1,8 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
-// @ts-ignore
-import router from './Routes/routes.js'
+import router from './Routes/routes'
 
-<<<<<<< HEAD
+
 const app = createApp(App)
 
 const globalState = reactive({
@@ -16,6 +13,3 @@ app.provide('globalState', globalState)
 app.use(router)
 
 app.mount('#app')
-=======
-createApp(App).mount('#app')
->>>>>>> parent of d9d43a1 (Create a global array for user roles)
