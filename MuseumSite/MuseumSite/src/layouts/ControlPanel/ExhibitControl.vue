9a45785f1c035565d84a45cfb42264c90f79540c
VMekh8 <mekhvitaliy88@gmail.com>
2024-05-01 15:32:55 +0300

    add table with exhibits

--- MuseumSite/MuseumSite/src/layouts/ControlPanel/ExhibitControl.vue
+++ MuseumSite/MuseumSite/src/layouts/ControlPanel/ExhibitControl.vue
@@ -7,8 +7,56 @@
         </div>
         
         <div class="table">
-            
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Назва</th>
+                  <th>Опис</th>
+                  <th>Зображення</th>
+                  <th>ID Виставки</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="exhibit in exhibits" :key="exhibit.id">
+                  <td>{{ exhibit.id }}</td>
+                  <td>{{ exhibit.title }}</td>
+                  <td>{{ exhibit.description }}</td>
+                  <td><img :src="'data:;base64,' + exhibit.image" /></td>
+                </tr>
+              </tbody>
+            </table>
+
         </div>
 
     </div>
 </template>
+
+<script lang="ts">
+import axios from "axios";
+import { ref, onMounted } from 'vue';
+import { ExhibitResponce } from '../../Models/Exhibit';
+
+export default {
+    setup() {
+        const exhibits = ref<ExhibitResponce[]>([]);
+        
+        const exhibitsFetch = async () => {
+
+            const response = await axios.get(import.meta.env.VITE_API_URL+'/Client/GetAllExhibit');
+
+            exhibits.value = response.data.map((exhibit: any) => new ExhibitResponce(
+                exhibit.id,
+                exhibit.title,
+                exhibit.description,
+                exhibit.image,
+            ));
+        };
+        
+        onMounted(exhibitsFetch);
+
+        return {exhibits};
+    }
+}
+
+</script>
