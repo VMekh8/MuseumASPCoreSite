1a698f8494458a030c9733229adf2271ac45becd
VMekh8 <mekhvitaliy88@gmail.com>
2024-05-14 14:01:18 +0300

    Create edit forms for Exhibitions

--- MuseumSite/MuseumSite/src/layouts/ControlPanel/ExhibitionControl.vue
+++ MuseumSite/MuseumSite/src/layouts/ControlPanel/ExhibitionControl.vue
@@ -18,14 +18,27 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="exhibition in exhibitions" :key="exhibition.id">
+          <tr v-for="(exhibition, index) in exhibitions" :key="exhibition.id">
             <td>{{ exhibition.id }}</td>
-            <td>{{ exhibition.name }}</td>
-            <td>{{ exhibition.description }}</td>
-            <td>{{ FormatDate(exhibition.EventDate.toString()) }}</td>
-            <td><img :src="'data:;base64,' + exhibition.image" /></td>
+            <td @dblclick="startEditing(index, 'name')">
+              <span v-if="!isEditName[index]">{{ exhibition.name }}</span>
+              <input v-else type="text" v-model="exhibition.name" @blur="stopEditing(index, 'name')">
+            </td>
+            <td @dblclick="startEditing(index, 'description')">
+              <span v-if="!isEditDesc[index]">{{ exhibition.description }}</span>
+              <input v-else type="text" v-model="exhibition.description" @blur="stopEditing(index, 'description')">
+            </td>
+            <td @dblclick="startEditing(index, 'eventDate')">
+              <span v-if="!isEditDate[index]">{{ exhibition.EventDate }}</span>
+              <input v-else type="datetime-local" v-model="exhibition.EventDate" @blur="stopEditing(index, 'eventDate')">
+            </td>
+            <td @dblclick="startEditing(index, 'image')">
+              <img v-if="!isEditImage[index] " :src="'data:;base64,' + exhibition.image" />
+          <input v-else type="file" @change="updateImage(index, $event)" />
+            </td>
+            
             <td>
-              <button>Редагувати</button>
+              <button @click="updateExhibition(exhibition.id)">Редагувати</button>
               <button @click="deleteExhibition(exhibition.id)">Видалити</button>
             </td>
             <td>
