68b387aad672678969ad3da2115abba6c6ea68e7
VMekh8 <mekhvitaliy88@gmail.com>
2024-05-15 20:29:29 +0300

    make a card for exhibit on exhibition get infromation

--- MuseumSite/MuseumSite/src/modals/ExhibitOnExhibitions.vue
+++ MuseumSite/MuseumSite/src/modals/ExhibitOnExhibitions.vue
@@ -9,8 +9,20 @@
         </b-input-group>
       </div>
       <div class="mb-3" v-for="exhibit in exhibits" :key="exhibit.id">
-        <h3>{{ exhibit.title }}</h3>
-        <p>{{ exhibit.description }}</p>
+        <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+           <b-row no-gutters>
+             <b-col md="6">
+                <b-card-img :src="'data:;base64,' + exhibit.image" alt="Image" class="rounded-0"></b-card-img>
+             </b-col>
+             <b-col md="6">
+               <b-card-body :title='exhibit.id + " " +exhibit.title'>
+                 <b-card-text>
+                   <p>{{ exhibit.description }}</p>
+                 </b-card-text>
+               </b-card-body>
+             </b-col>
+           </b-row>
+         </b-card>
       </div>
       <div class="d-flex justify-content-end">
         <b-button variant="primary" @click="onOk">OK</b-button>
